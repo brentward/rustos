381c9dfad58a5efc7dd62f8d40978bc73a3788f8
Brent Bettis <bbettis@expedia.com>
2020-02-28 11:40:26 -0700

    porting allocator utils

--- kern/src/allocator/util.rs
+++ kern/src/allocator/util.rs
@@ -6,7 +6,8 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    assert!(align.is_power_of_two());
+    addr & !(align - 1)
 }
 
 /// Align `addr` upwards to the nearest multiple of `align`.
@@ -18,5 +19,23 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 /// Panics if `align` is not a power of 2
 /// or aligning up overflows the address.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    align_down(addr + align - 1, align)
+}
+
+/// Returns `true` if `addr` is aligned to  `align` and `false` otherwise.
+pub fn has_alignment(addr: usize, align: usize) -> bool {
+    addr == addr & !(align - 1)
+}
+
+/// Takes one pointer `addr` and `size` and returns a tupple two pointers
+/// of type `*mut usize` where the first is the size of size and the second is the remaining size
+///
+/// # Saftey
+///
+/// The caller must ensure that `addr` refers to unique, writeable memory at
+/// least `size` in size.
+pub unsafe fn split_addr(addr: *mut usize, size: usize) -> (*mut usize, *mut usize) {
+    let new_addr = addr as usize + size;
+    let new_ptr = new_addr as *mut usize;
+    (addr, new_ptr)
 }
