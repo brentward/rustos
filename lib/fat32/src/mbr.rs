76f19cb62397f57abb15543da7613061c4edeeb1
Brent Bettis <bbettis@expedia.com>
2020-02-28 18:52:57 -0700

    MBR implementation

--- lib/fat32/src/mbr.rs
+++ lib/fat32/src/mbr.rs
@@ -2,24 +2,62 @@ use core::fmt;
 use shim::const_assert_size;
 use shim::io;
 
+use core::mem;
+
 use crate::traits::BlockDevice;
 
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct CHS {
+    head: u8,
+    sector: u8,
+    cylinder: u8,
+    // sector_cylinder: u16,
+
     // FIXME: Fill me in.
 }
 
-// FIXME: implement Debug for CHS
+impl fmt::Debug for CHS {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let sector_cylinder: u16 = ((self.sector as u16) << 8) & self.cylinder as u16;
+        let sector = sector_cylinder & !(0b1111111111 << 6);
+        let cylinder = sector_cylinder >> 6;
+        f.debug_struct("CHS")
+            .field("head", &self.head)
+            .field("sector", &sector)
+            .field("cylinder", &cylinder)
+
+            .finish()
+    }
+}
 
 // const_assert_size!(CHS, 3);
 
 #[repr(C, packed)]
 pub struct PartitionEntry {
+    boot_flag: u8,
+    start_chs: CHS,
+    partition_type: u8,
+    end_chs: CHS,
+    start_sector: u32,
+    total_sectors: u32,
+
     // FIXME: Fill me in.
 }
 
-// FIXME: implement Debug for PartitionEntry
+impl fmt::Debug for PartitionEntry {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("PartitionEntry")
+            .field("boot flag", &self.boot_flag)
+            .field("start CHS", &self.start_chs)
+            .field("partition type", &self.partition_type)
+            .field("end CHS", &self.end_chs)
+            .field("start sector", &self.start_sector)
+            .field("total sector", &self.total_sectors)
+
+            .finish()
+    }
+}
 
 // const_assert_size!(PartitionEntry, 16);
 
@@ -27,10 +65,26 @@ pub struct PartitionEntry {
 #[repr(C, packed)]
 pub struct MasterBootRecord {
     // FIXME: Fill me in.
+    bootstrap: [u8; 436],
+    disk_id: [u8; 10],
+    partitions: [PartitionEntry; 4],
+    magic_signature: [u8; 2],
+
 }
 
-// FIXME: implemente Debug for MaterBootRecord
+impl fmt::Debug for MasterBootRecord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("MasterBootRecord")
+            .field("disk id", &self.disk_id)
+            .field("partition 1", &self.partitions[0])
+            .field("partition 2", &self.partitions[1])
+            .field("partition 3", &self.partitions[2])
+            .field("partition 4", &self.partitions[3])
+            .field("magic signature", &self.magic_signature)
 
+            .finish()
+    }
+}
 // const_assert_size!(MasterBootRecord, 512);
 
 #[derive(Debug)]
@@ -53,6 +107,22 @@ impl MasterBootRecord {
     /// boot indicator. Returns `Io(err)` if the I/O error `err` occured while
     /// reading the MBR.
     pub fn from<T: BlockDevice>(mut device: T) -> Result<MasterBootRecord, Error> {
-        unimplemented!("MasterBootRecord::from()")
+        // let sector_size = device.sector_size();
+        let mut buf = [0u8; 512];
+        match device.read_sector(0, &mut buf) {
+            Err(err) => return Err(Error::Io(err)),
+            Ok(bytes) if bytes > 512 => return Err(Error::BadSignature),
+            _ => (),
+        }
+        let mbr: MasterBootRecord = unsafe { mem::transmute(buf) };
+        if mbr.magic_signature != [0x55u8, 0xAAu8] {
+            return Err(Error::BadSignature);
+        }
+        for (index, partition) in mbr.partitions.iter().enumerate() {
+            if !(partition.boot_flag == 0 || partition.boot_flag == 0x80) {
+                return Err(Error::UnknownBootIndicator(index as u8));
+            }
+        }
+        Ok(mbr)
     }
 }
