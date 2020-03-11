bc661a809c7cd732dbe4663f97c4ac659d15755c
Brent Bettis <bbettis@expedia.com>
2020-03-11 15:45:11 -0700

    cleaning up shell, improving error handling, standardizing result writing

--- kern/src/shell.rs
+++ kern/src/shell.rs
@@ -8,6 +8,7 @@ use pi::atags::Atags;
 use alloc::string::{String, ToString};
 use alloc::vec;
 use alloc::vec::Vec;
+use core::fmt::{self, Write as FmtWrite};
 
 use fat32::traits::FileSystem;
 use fat32::traits::{Dir, Entry, Metadata};
@@ -109,15 +110,6 @@ pub fn shell(prefix: &str) -> ! {
                     "cd" => Cd::exec(&command, &mut cwd),
                     "ls" => Ls::exec(&command, &mut cwd),
                     "cat" => Cat::exec(&command, &mut cwd),
-                    // "pwd" => pwd(),
-                    // "cd" => cd(&command.args),
-                    // "ls" => ls(&command.args),
-                    // "cat" => cat(&command.args),
-                    // "atags" => atag(&command.args),
-                    // "panic" => panic!("You called panic"),
-                    // "unreachable" => unreachable!(),
-                    // "usemem" => use_memory(),
-                    // "memstats" => memstats(),
                     _path => Unknown::exec(&command, &mut cwd),
                 };
                 match exec_result {
@@ -160,12 +152,12 @@ impl Executable for Echo {
     fn exec(cmd: &Command, _cwd: &mut PathBuf) ->Result<StdOut, StdErr> {
         let mut result = String::new();
         for &arg in cmd.args[1..].iter() {
-            result.push_str(arg);
-            result.push(' ');
+            write!(result, "{} ", arg);
         }
         if result.len() > 0 {
             result.pop();
         }
+        writeln!(result, "");
 
         Ok(StdOut { result, code: 0 })
     }
@@ -175,9 +167,8 @@ struct Unknown;
 
 impl Executable for Unknown {
     fn exec(cmd: &Command, _cwd: &mut PathBuf) -> Result<StdOut, StdErr> {
-        let mut result = String::from("bwsh: command not found: ");
-        result.push_str(cmd.path());
-        result.push_str("\r\n");
+        let mut result = String::new();
+        writeln!(result, "bwsh: command not found: {}", cmd.path());
 
         Err(StdErr { result, code: 1 })
     }
@@ -187,14 +178,14 @@ struct Pwd;
 
 impl Executable for Pwd {
     fn exec(cmd: &Command, cwd: &mut PathBuf) ->Result<StdOut, StdErr> {
+        let mut result = String::new();
         if cmd.args.len() != 1 {
-            let result = String::from("pwd: too many arguments\r\n");
+            writeln!(result, "pwd: too many arguments");
+
             Err(StdErr { result, code: 1 })
         } else {
-            let path = cwd.as_path().to_str().expect("path is not valid unicode");
-            let mut result = String::from(path);
-            result.push_str("\r\n");
-            // let result = String::from(cwd.to_str());
+            writeln!(result, "{}", cwd.as_path().to_str().expect("path is not valid unicode"));
+
             Ok(StdOut { result, code: 0 })
         }
     }
@@ -204,18 +195,15 @@ struct Cd;
 
 impl Executable for Cd {
     fn exec(cmd: &Command, cwd: &mut PathBuf) ->Result<StdOut, StdErr> {
+        let mut result = String::new();
         let mut path = Path::new("/");
         let mut working_dir = cwd.clone();
         if cmd.args.len() > 2 {
-            let result = String::from("cd: too many arguments\r\n");
+            writeln!(result, "cd: too many arguments");
+
             return Err(StdErr { result, code: 1 });
         } else if cmd.args.len() == 2 {
             path = Path::new(cmd.args[1]);
-        // } else {
-        //     while cwd.pop() {
-        //         cwd.pop();
-        //     }
-        //     return Ok(StdOut { result: String::new(), code: 0})
         }
         if path.is_absolute() {
             while working_dir.pop() {
@@ -232,15 +220,11 @@ impl Executable for Cd {
             }
         }
 
-        // working_dir.push(path);
-
         let entry = match FILESYSTEM.open(working_dir.as_path()) {
             Ok(entry) => entry,
             Err(_) => {
-                let mut result = String::from("cd: no such file or directory: ");
-                result.push_str( path.to_str()
-                    .expect("path is not valid unicode"));
-                result.push_str("\r\n");
+                writeln!(result, "cd: no such file or directory: {}", path.to_str().unwrap());
+
                 return Err(StdErr { result, code: 1 });
             }
         };
@@ -250,16 +234,13 @@ impl Executable for Cd {
                 while cwd.pop() {
                     cwd.pop();
                 }
-
                 cwd.push(working_dir.as_path());
-                Ok(StdOut { result: String::new(), code: 0 })
-                //exists, set dir
+
+                Ok(StdOut { result, code: 0 })
             }
             None => {
-                let mut result = String::from("cd: not a directory: ");
-                result.push_str( path.to_str()
-                    .expect("path is not valid unicode"));
-                result.push_str("\r\n");
+                writeln!(result, "cd: not a directory: {}", path.to_str().unwrap());
+
                 Err(StdErr { result, code: 1 })
             }
         }
@@ -270,29 +251,66 @@ struct Ls;
 
 impl Executable for Ls {
     fn exec(cmd: &Command, cwd: &mut PathBuf) ->Result<StdOut, StdErr> {
+        fn get_size(size: usize, human_redable: bool) -> String {
+            let mut result = String::new();
+            if human_redable {
+                match size {
+                    size@ 0..=1023 => {
+                        write!(result, "{} B", size.to_string());
+                    }
+                    size@ 1024..=1_048_575 => {
+                        write!(result, "{} KiB", (size / 1024).to_string());
+                    }
+                    size@ 1_048_576..=1_073_741_823 => {
+                        write!(result, "{} MiB", (size / 1_048_576).to_string());
+                    }
+                    size => {
+                        write!(result, "{} GiB", (size / 1_073_741_824).to_string());
+                    }
+                }
+            } else {
+                write!(result, "{}", size);
+            }
+            result
+        }
+
+        let mut result = String::new();
         let mut option_end = cmd.args.len();
         let mut show_hidden = false;
+        let mut human_readable = false;
         if cmd.args.len() > 1 {
             for arg_index in 1..cmd.args.len() {
-                if &cmd.args[arg_index][..1] != "-" {
+                if cmd.args[arg_index].len() > 2 && &cmd.args[arg_index][..2] == "--" {
+                    match &cmd.args[arg_index][2..] {
+                        "all" => show_hidden = true,
+                        "human-readable" => human_readable = true,
+                        option => {
+                            writeln!(result, "ls: invalid option: --{}", option);
+
+                            return Err(StdErr { result, code: 1 });
+                        }
+                    }
+                } else if cmd.args[arg_index].len() > 1 && &cmd.args[arg_index][..1] == "-" {
+                    for arg in cmd.args[arg_index][1..].chars() {
+                        match arg {
+                            'a' => show_hidden = true,
+                            'h' => human_readable = true,
+                            option => {
+                                writeln!(result, "ls: invalid option: -{}", option);
+
+                                return Err(StdErr { result, code: 1 });
+                            }
+                        }
+                    }
+                } else {
                     option_end = arg_index;
                     break
                 }
             }
         }
-        for param in cmd.args[1..option_end].iter() {
-            match param {
-                &"-a" => show_hidden = true,
-                &option => {
-                    let mut result = String::from("ls: invalid option: ");
-                    result.push_str(option);
-                    result.push_str("\r\n");
-                    return Err(StdErr { result, code: 1 });
-                }
-            }
-        }
         if cmd.args.len() > option_end + 1 {
-            let result = String::from("ls: too many arguments\r\n");
+            writeln!(result, "ls: too many arguments");
+
             return Err(StdErr { result, code: 1 });
         }
         let path = if cmd.args.len() > option_end {
@@ -321,14 +339,12 @@ impl Executable for Ls {
         let entry = match FILESYSTEM.open(working_dir.as_path()) {
             Ok(entry) => entry,
             Err(_) => {
-                let mut result = String::from("cd: no such file or directory: ");
-                result.push_str( path.to_str()
+                writeln!(result, "cd: no such file or directory: {}", path.to_str()
                     .expect("path is not valid unicode"));
-                result.push_str("\r\n");
+
                 return Err(StdErr { result, code: 1 });
             }
         };
-        let mut result = String::new();
         match entry.as_dir() {
             Some(dir) => {
                 let entries = dir.entries().unwrap().collect::<Vec<_>>();
@@ -341,18 +357,9 @@ impl Executable for Ls {
                         } else {
                             String::from(entry.name())
                         };
-                        let size = get_size(entry.size());
-                        let mut size_spacer = String::new();
-                        for _ in 0..(8 - size.len()) {
-                            size_spacer.push(' ');
-                        }
-                        result.push_str(entry.metadata().to_string().as_str());
-                        result.push_str("   ");
-                        result.push_str(size.as_str());
-                        result.push_str(size_spacer.as_str());
-                        result.push_str("   ");
-                        result.push_str(name.as_str());
-                        result.push_str("\r\n");
+                        let metadata = entry.metadata().to_string();
+                        let size = get_size(entry.size(), human_readable);
+                        write!(result, "{}  {:<8}  {} \r\n", metadata, size, name);
                     }
                 }
             }
@@ -365,18 +372,9 @@ impl Executable for Ls {
                     } else {
                         String::from(entry.name())
                     };
-                    let size = get_size(entry.size());
-                    let mut size_spacer = String::new();
-                    for _ in 0..(8 - size.len()) {
-                        size_spacer.push(' ');
-                    }
-                    result.push_str(entry.metadata().to_string().as_str());
-                    result.push_str("   ");
-                    result.push_str(size.as_str());
-                    result.push_str(size_spacer.as_str());
-                    result.push_str("   ");
-                    result.push_str(name.as_str());
-                    result.push_str("\r\n");
+                    let metadata = entry.metadata().to_string();
+                    let size = get_size(entry.size(), human_readable);
+                    write!(result, "{}  {:<10} {} \r\n", metadata, size, name);
                 }
             }
         }
@@ -394,6 +392,13 @@ impl Executable for Cat {
             let mut working_dir = cwd.clone();
 
             let path = Path::new(&arg);
+
+            if path.is_absolute() {
+                while working_dir.pop() {
+                    working_dir.pop();
+                }
+            }
+
             for dir in path.iter() {
                 if dir.to_str().unwrap() == "." {
                 } else if dir.to_str().unwrap() == ".." {
@@ -403,20 +408,12 @@ impl Executable for Cat {
                 }
             }
 
-            if path.is_absolute() {
-                while working_dir.pop() {
-                    working_dir.pop();
-                }
-            }
-
             let entry = match FILESYSTEM.open(working_dir.as_path()) {
                 Ok(entry) => entry,
                 Err(_) => {
-                    let mut result = String::from("cat: ");
-                    result.push_str( path.to_str()
+                    writeln!(&mut result, "cat: {} no such fhe or directory", path.to_str()
                         .expect("path is not valid unicode"));
-                    result.push_str(": no such file or directory");
-                    result.push_str("\r\n");
+
                     return Err(StdErr { result, code: 1 });
                 }
             };
@@ -427,11 +424,9 @@ impl Executable for Cat {
             let mut file = match entry.into_file() {
                 Some(file) => file,
                 None => {
-                    let mut result = String::from("cat: ");
-                    result.push_str(path.to_str()
+                    writeln!(result, "cat: {}: is a directory", path.to_str()
                         .expect("path is not valid unicode"));
-                    result.push_str(": is a directory");
-                    result.push_str("\r\n");
+
                     return Err(StdErr { result, code: 1 });
                 }
             };
@@ -440,15 +435,13 @@ impl Executable for Cat {
                 let bytes = match file.read(&mut buf) {
                     Ok(bytes) => bytes,
                     Err(_) => {
-                        let mut result = String::from("cat: ");
-                        result.push_str( path.to_str()
+                        writeln!(result, "cat: {}: file could not be opened", path.to_str()
                             .expect("path is not valid unicode"));
-                        result.push_str(": file could not be opened");
-                        result.push_str("\r\n");
+
                         return Err(StdErr { result, code: 1 });
                     }
                 };
-                let bytes_written = file_vec.write(&buf)
+                let _bytes_written = file_vec.write(&buf)
                     .expect("failed to write to vector");
                 bytes_read += bytes;
             }
@@ -457,109 +450,17 @@ impl Executable for Cat {
             }
             match String::from_utf8(file_vec) {
                 Ok(string) => {
-                    result.push_str(string.as_str());
-                    result.push_str("\r\n");
+                    writeln!(result, "{}", string.as_str());
                 },
                 Err(_) => {
-                    let mut result = String::from("cat: ");
-                    result.push_str( path.to_str()
+                    writeln!(result, "cat: {}: file not valid UTF-8", path.to_str()
                         .expect("path is not valid unicode"));
-                    result.push_str(": file not valid UTF-8");
-                    result.push_str("\r\n");
+
                     return Err(StdErr { result, code: 1 });
                 }
             }
         }
 
         Ok(StdOut { result, code: 0 })
-    }
-}
-
-fn atag(args: &StackVec<&str>) {
-    let atags = Atags::get();
-    if args.len() > 1 {
-        match args[1] {
-            "mem" => {
-                for atag in atags {
-                    match atag {
-                        pi::atags::Atag::Mem(_) => kprintln!("{:#?}", atag),
-                        _ => (),
-                    }
-                }
-            }
-            "core" => {
-                for atag in atags.into_iter() {
-                    match atag {
-                        pi::atags::Atag::Core(_) => kprintln!("{:#?}", atag),
-                        _ => (),
-                    }
-                }
-            }
-            "cmd" => {
-                for atag in atags.into_iter() {
-                    match atag {
-                        pi::atags::Atag::Cmd(_) => kprintln!("{:#?}", atag),
-                        _ => (),
-                    }
-                }
-            }
-            "unknown" => {
-                for atag in atags.into_iter() {
-                    match atag {
-                        pi::atags::Atag::Unknown(_) => kprintln!("{:#?}", atag),
-                        _ => (),
-                    }
-                }
-            }
-            _ => {
-                for atag in atags.into_iter() {
-                    kprintln!("{:#?}", atag);
-                }
-            }
-        }
-    } else {
-        for atag in atags.into_iter() {
-            kprintln!("{:#?}", atag);
-        }
-    }
-}
-
-fn use_memory() {
-    let mut base_string = String::from("hi again");
-    let mut string_vec = vec![base_string.clone()];
-    for _ in 0..1024 {
-        base_string.push_str(", and again");
-        let new_string = base_string.clone();
-        string_vec.push(new_string);
-    };
-    kprintln!("{:?}", string_vec[1023]);
-}
-
-fn memstats() {
-    kprintln!{"{:?}", ALLOCATOR};
-}
-
-fn get_size(size: usize) -> String {
-    match size {
-        size@ 0..=1023 => {
-            let mut size_str = size.to_string();
-            size_str.push_str(" B");
-            size_str
-        },
-        size@ 1024..=1_048_575 => {
-            let mut size_str = (size / 1024).to_string();
-            size_str.push_str(" KiB");
-            size_str
-        },
-        size@ 1_048_576..=1_073_741_823 => {
-            let mut size_str = (size / 1_048_576).to_string();
-            size_str.push_str(" MiB");
-            size_str
-        },
-        size => {
-            let mut size_str = (size / 1_073_741_824).to_string();
-            size_str.push_str(" GiB");
-            size_str
-        },
     }
 }
