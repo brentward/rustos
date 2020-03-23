8ee2d41f2cb9968a8c32ac75cede250692e3ffbf
Brent Bettis <bbettis@expedia.com>
2020-03-22 22:48:10 -0700

    Adding virtual memory page table registers to trap frame

--- kern/src/init/vectors.s
+++ kern/src/init/vectors.s
@@ -30,12 +30,15 @@ context_save:
     stp q4, q5, [SP, #-32]!
     stp q2, q3, [SP, #-32]!
     stp q0, q1, [SP, #-32]!
-    mrs x9, TPIDR_EL0
-    mrs x8, SP_EL0
-    stp x8, x9, [SP, #-16]!
-    mrs x9, SPSR_EL1
-    mrs x8, ELR_EL1
-    stp x8, x9, [SP, #-16]!
+    mrs x10, TPIDR_EL0
+    mrs x9, SP_EL0
+    stp x9, x10, [SP, #-16]!
+    mrs x10, SPSR_EL1
+    mrs x9, ELR_EL1
+    stp x9, x10, [SP, #-16]!
+    mrs x10, TTBR1_EL1
+    mrs x9, TTBR0_EL1
+    stp x9, x10, [SP, #-16]!
     mov x0, x29
     mrs x1, ESR_EL1
     mov x2, SP
@@ -45,12 +48,19 @@ context_save:
 
 .global context_restore
 context_restore:
-    ldp  x8, x9, [SP], #16
-    msr ELR_EL1, x8
-    msr SPSR_EL1, x9
-    ldp  x8, x9, [SP], #16
-    msr SP_EL0, x8
-    msr TPIDR_EL0, x9
+    ldp x9, x10, [SP], #16
+    msr TTBR0_EL1, x9
+    msr TTBR1_EL1, x10
+    dsb ishst
+    tlbi vmalle1
+    dsb ish
+    isb
+    ldp x9, x10, [SP], #16
+    msr ELR_EL1, x9
+    msr SPSR_EL1, x10
+    ldp x9, x10, [SP], #16
+    msr SP_EL0, x9
+    msr TPIDR_EL0, x10
     ldp q0, q1, [SP], #32
     ldp q2, q3, [SP], #32
     ldp q4, q5, [SP], #32
--- kern/src/traps/frame.rs
+++ kern/src/traps/frame.rs
@@ -3,6 +3,7 @@ use core::fmt;
 #[repr(C)]
 #[derive(Default, Copy, Clone, Debug)]
 pub struct TrapFrame {
+    pub ttbr: [u64; 2],
     pub elr: u64,
     pub spsr: u64,
     pub sp: u64,
