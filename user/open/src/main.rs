fd041ff74be1a1f4d7a124ee764252fe407af199
Brent Bettis <bbettis@expedia.com>
2020-04-04 22:17:53 -0700

    Adding read file to the user process created by open.bin

--- user/open/src/main.rs
+++ user/open/src/main.rs
@@ -13,11 +13,13 @@ pub static ALLOCATOR: Allocator = Allocator::uninitialized();
 mod cr0;
 
 use kernel_api::println;
-use kernel_api::syscall::{open, exit};
+use kernel_api::syscall::{open, exit, read};
 
 use core::time::Duration;
 use alloc::string::String;
 use alloc::vec;
+use alloc::vec::Vec;
+use shim::io::Write;
 
 
 fn fib(n: u64) -> u64 {
@@ -29,16 +31,43 @@ fn fib(n: u64) -> u64 {
 }
 
 fn main() {
-    match open("/config.txt") {
-        Ok(fid) => println!("fid: {}", fid),
+    let open_result =  open("/nerdfonts.txt");
+    match open_result {
+        Ok(fid) => {
+            let mut file_vec = Vec::new();
+            let mut bytes = 0usize;
+            let mut bytes_read = 0usize;
+            loop {
+                let mut file_buf = [0u8; 256];
+                bytes = match read(fid, &mut file_buf){
+                    Ok(bytes) => bytes,
+                    Err(e) => {
+                        println!("{:?}", e);
+                        0
+                    }
+                };
+                if bytes == 0 {
+                    break
+                }
+                bytes_read += bytes;
+                let _bytes_written = file_vec.write(&file_buf)
+                    .expect("failed to write to vector");
+
+
+
+            }
+            while file_vec.len() > bytes_read {
+                file_vec.pop();
+            }
+            match String::from_utf8(file_vec) {
+                Ok(string) => {
+                    println!("{}", string);
+                },
+                Err(_) => println!("Error converting file_vec to string"),
+            };
+
+
+        },
         Err(e) => println!("{:#?}", e),
     }
-
-    let foo = vec![1, 2, 3];
-    println!("foo is {:?}", foo);
-    let mut bar = String::from("This is a string and it is on the heap");
-    println!("bar is {}", bar);
-    bar.push_str(". And I added this to the string!");
-    println!("now bar is: {}", bar);
-
 }
