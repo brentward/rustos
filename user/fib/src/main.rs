19f2e4f99ca7d8488289a5b8f57001163dbd9135
Brent Bettis <bbettis@expedia.com>
2020-04-04 22:19:14 -0700

    restoring fib.bin to original

--- user/fib/src/main.rs
+++ user/fib/src/main.rs
@@ -5,7 +5,7 @@
 mod cr0;
 
 use kernel_api::println;
-use kernel_api::syscall::{getpid, time, sleep, exit};
+use kernel_api::syscall::{getpid, time};
 
 use core::time::Duration;
 
@@ -20,30 +20,7 @@ fn fib(n: u64) -> u64 {
 fn main() {
     println!("Started...");
 
-    let pid = getpid();
-    println!("PID: {}", pid);
-
-    let current_time = time();
-    println!("time in milliseconds: {}", current_time.as_millis());
-
-
     let rtn = fib(40);
 
     println!("Ended: Result = {}", rtn);
-
-    let current_time = time();
-    println!("time in milliseconds: {}", current_time.as_millis());
-
-    println!("sleep for 5 sec");
-    sleep(Duration::from_secs(5));
-
-    println!("I'm back");
-    println!("another fib");
-    let rtn = fib(40);
-
-    println!("Ended: Result = {}", rtn);
-
-    println!("goodbye");
-
-    exit()
 }
